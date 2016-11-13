a878fd970cec9cb1c35195af62b19e8aeb43e018
Peter Jang <peter.x.jang@gmail.com>
2016-11-13 09:37:25 -0600

    Add index, show, new, and create actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,27 @@
 class RecipesController < ApplicationController
-  def first_recipe_method
-    @recipe = Recipe.first
-    render 'first_recipe.html.erb'
+  def index
+    @recipes = Recipe.all
+    render 'index.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    recipe = Recipe.new(
+      title: params["title"],
+      chef: params["chef"],
+      ingredients: params["ingredients"],
+      directions: params["directions"]
+    )
+    recipe.save
+    render 'create.html.erb'
+  end
+
+  def show
+    recipe_id = params[:id]
+    @recipe = Recipe.find_by(id: recipe_id)
+    render 'show.html.erb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/first_recipe_url' => 'recipes#first_recipe_method'
+  get '/recipes' => 'recipes#index'
+  get '/recipes/new' => 'recipes#new'
+  post '/recipes' => 'recipes#create'
+  get '/recipes/:id' => 'recipes#show'
 end
